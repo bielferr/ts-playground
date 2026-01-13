99c89a7d0b35f927ad29ea436b3ffcfaf67a077d
bielferr <gabrielferreiradesouza5513@gmail.com>
2026-01-13 09:11:35 -0300

    variaveis de ambiente

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express from 'express'
 import 'express-async-errors'
 import cors from 'cors'
@@ -12,6 +13,6 @@ app.get('/', (req, res) => {
   return res.json({ message: 'Hello, world!' })
 })
 
-app.listen(5555, () => {
-  console.log('Server is running on port : http://localhost:5555✅')
+app.listen(process.env.PORT, () => {
+  console.log(`message: Server is running on port ${process.env.PORT}!!!✅`)
 })
