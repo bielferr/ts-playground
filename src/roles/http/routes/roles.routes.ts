dccb910d14be6186a8baec5bcc5912a2064311d4
bielferr <gabrielferreiradesouza5513@gmail.com>
2026-01-14 09:35:29 -0300

    Add role entitty

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,18 +1,21 @@
+import { Role } from '@roles/entities/roles'
 import { Router } from 'express'
-import { v4 as uuidv4 } from 'uuid'
 
 const rolesRoutes = Router()
 
-const roles = []
+const roles: Role[] = []
 // Defina suas rotas relacionadas a "roles" aqui
 
 rolesRoutes.post('/', (req, res) => {
   const { name } = req.body
-  const role = {
-    id: uuidv4(),
+
+  const role = new Role()
+
+  Object.assign(role, {
     name,
     created_at: new Date(),
-  }
+  })
+
   roles.push(role)
   return res.status(201).json(role)
 })
