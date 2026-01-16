3b0bc6f58360ec8c408bcb1c59d17fd4c9e93e4c
bielferr <gabrielferreiradesouza5513@gmail.com>
2026-01-16 08:58:19 -0300

    crate migration create roles table

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1768563328523 } from './migrations/1768563328523-CreateRolesTable'
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1768563328523],
 })
